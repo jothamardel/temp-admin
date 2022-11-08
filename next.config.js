9d57f48644efb50512987ebad73a5d48570ce077
jothamardel <ardelmbiplang@gmail.com>
2022-11-08 23:04:32 +0100

    bug: updating next.config.js settings to fix deployment error

--- next.config.js
+++ next.config.js
@@ -8,11 +8,11 @@ module.exports = withPWA({
     
   // },
   // i18n,
-  pwa: {
-    disable: process.env.NODE_ENV === "development",
-    dest: "public",
-    runtimeCaching,
-  },
+  // pwa: {
+  disable: process.env.NODE_ENV === "development",
+  dest: "public",
+  runtimeCaching,
+  // },
 
   images: {
     domains: [
